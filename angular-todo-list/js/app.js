04df21a14a3635a4aa7e4fc1d5b49e1ce0b14bd5
Marcin <marcinmu330@gmail.com>
2016-09-15 15:16:39 -0500

    Added the initial files to this project.

---
+++ angular-todo-list/js/app.js
@@ -0,0 +1,6 @@
+﻿angular.module("todoListApp", [])
+.controller('mainCtrl', function ($scope) {
+    $scope.helloWorld = function () {
+        console.log("Hello there! This the helloWorld controller function, in the mainCtrl!");
+    };
+});
